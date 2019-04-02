8fdab4204346be81ef08b518d68a27af073f52d9
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-04-02 22:24:16 +0300

    computing final dist using analytic method

--- lab 2/lab2.py
+++ lab 2/lab2.py
@@ -22,25 +22,32 @@ def get_random_transition_matrix(n):
         g.append(get_random_norm_vector(n))
     return np.array(g)
 
+def vector_stdev(va, vb):
+    return np.sqrt(sum((b - a)**2 for b, a in zip(vb, va)))
 
-def limit_dist_by_computing(transition_matrix, stationary_dist, eps=1e-7):
+
+def limit_dist_by_computing(transition_matrix, stationary_dist, eps=1e-5):
     m_stationary_dist = stationary_dist ** 2
     stdev = vector_stdev(stationary_dist, m_stationary_dist)
     m = 0
     while stdev > eps:
         m_stationary_dist = stationary_dist @ transition_matrix
         m += 1
-        # print(stationary_dist, m_stationary_dist)
         stdev = vector_stdev(stationary_dist, m_stationary_dist)
-        # print("Отклонение", stdev)
         stationary_dist = m_stationary_dist
     return stationary_dist, m
 
 
-def vector_stdev(va, vb):
-    return np.sqrt(sum((b - a)**2 for b, a in zip(vb, va)))
+def limit_dist_by_analytic(transition_matrix):
+    A = (transition_matrix - np.eye(n)).transpose()
+    probability_dist = np.ones((1, n))
 
+    A = np.vstack((A, probability_dist))
+    B = np.zeros(n + 1)
+    B[-1] = 1
 
+    p = np.linalg.lstsq(A, B, rcond=1)[0]
+    return p
 
 
 if __name__ == '__main__':
@@ -48,20 +55,19 @@ if __name__ == '__main__':
     Markov_matrix = get_random_transition_matrix(n)
     print("Матрица переходов\n", Markov_matrix)
 
+    # Численный способ
     start_dist_1 = get_random_norm_vector(n)
 
-    print("Вектор начального состояния\n", start_dist_1)
-    print("Предельная матрица\n")
+    print("\nВектор начального состояния 1\n", start_dist_1)
+    print("\nЧисленное распределение 1")
     print(limit_dist_by_computing(Markov_matrix, start_dist_1))
 
     start_dist_2 = get_random_norm_vector(n)
-    print("Вектор начального состояния\n", start_dist_2)
-    print("Предельная матрица\n")
+    print("\nВектор начального состояния 2\n", start_dist_2)
+    print("\nЧисленное распределение 2")
     print(limit_dist_by_computing(Markov_matrix, start_dist_2))
 
-    print("Матрица в степени\n", np.linalg.matrix_power(Markov_matrix, n))
+    print("\nМатрица в степени m\n", np.linalg.matrix_power(Markov_matrix, n))
 
-
-    # A = (Markov_matrix.transpose() - np.eye(8))
-    # B = np.zeros((8, 1))
-    # print(np.linalg.inv(A) @ B)
+    # Аналитический способ
+    print("\nАналитическое распределение\n", limit_dist_by_analytic(Markov_matrix))
