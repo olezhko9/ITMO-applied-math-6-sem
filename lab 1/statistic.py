26659844cbe037bb33ba2d5cc2ea1ef70cf8db1a
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-04-04 02:25:35 +0300

    add pirson criteria and clean the code

--- lab 1/statistic.py
+++ lab 1/statistic.py
@@ -3,14 +3,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as st
 
-# https://www.ibm.com/support/knowledgecenter/ru/SSEP7J_10.1.1/com.ibm.swg.ba.cognos.ug_cr_rptstd.10.1.1.doc/c_id_obj_desc_tables.html
 
 def quartil(arr):
-    # квартили
+    """квартили"""
     q1, q2, q3 = 0, 0, 0
     arr = sorted(arr)
     q2 = np.median(arr)
-    mid = int(np.floor(len(arr)/2))
+    mid = int(np.floor(len(arr) / 2))
     if len(arr) % 2 == 0:
         q1 = np.median(arr[:mid + 1])
         q3 = np.median(arr[mid:])
@@ -20,65 +19,110 @@ def quartil(arr):
 
     return q1, q2, q3
 
+
 def stdev(arr):
-    # Стандартное отклонение
+    """Стандартное отклонение"""
     mean = np.mean(arr)
-    return np.sqrt(sum([(x - mean)**2 for x in arr]) / (len(arr)-1))
+    return np.sqrt(sum([(x - mean) ** 2 for x in arr]) / (len(arr) - 1))
+
 
 def sem(arr):
-    # Стандартная ошибка
+    """Стандартная ошибка"""
     return stdev(arr) / np.sqrt(len(arr))
 
+
 def kurtosis(arr):
-    # эксцесс
-    return sum((arr - np.mean(arr))**4) / len(arr) / stdev(stat)**4 - 3
+    """эксцесс"""
+    return sum((arr - np.mean(arr)) ** 4) / len(arr) / stdev(stat) ** 4 - 3
+
 
 def assimetria(arr):
-    return sum((arr - np.mean(arr))**3) / len(arr) / stdev(stat)**3
+    return sum((arr - np.mean(arr)) ** 3) / len(arr) / stdev(stat) ** 3
 
-def pirson(arr):
-    k = 10
+
+def laplass(x):
+    return st.norm.cdf(x) - 0.5
+
+
+def pirson(arr, a, k, chi_crit):
+    """
+    Вычисляет хи-квадрат для заданной выборке
+    :param arr: массив чисел - выборка
+    :param a: уровень значимости
+    :param k: количество интервалов разбиения
+    :param chi_crit: хи-квадрат критическое при аргументах (k-2-1, a)
+    :return: True, если совокупность подчиняется нормальному закону, иначе False.
+    Также возвращает вычисленное значение хи-квадрат.
+    """
+    _k = k
     _min = np.min(arr)
     _max = np.max(arr)
-    h = (_max - _min) / k
+    h = (_max - _min) / _k
+
     x = _min
-    i = 1
+    k_intervals = {}
     while x < _max:
         xi = np.round(x + h, 3)
-        print(i, x, xi)
-        i += 1
+        k_intervals[(x, xi)] = {'ni': 0}
         x = xi
 
-    return h, _min, _max
+    for a in arr:
+        for interval in k_intervals:
+            if interval[0] <= a <= interval[1]:
+                k_intervals[interval]['ni'] += 1
+                break
 
-def interval(arr):
+    mean = np.mean(arr)
+    std = stdev(arr)
+    chisquare = 0
+    for interval in k_intervals:
+        x1 = (interval[0] - mean) / std
+        x2 = (interval[1] - mean) / std
+        k_intervals[interval]["pi"] = laplass(x2) - laplass(x1)
+        chisquare += ((k_intervals[interval]['ni'] - len(arr) * k_intervals[interval]["pi"]) ** 2) / \
+                     (len(arr) * k_intervals[interval]["pi"])
+
+    if chisquare > chi_crit:
+        return False, chisquare
+    else:
+        return True, chisquare
+
+
+def confidence_interval(arr):
+    """Доверительные интервалы"""
     # для математического ожидания
-    t = 1.96    # аргумент функции Лапласса, при котором ее значение равно 0,95 / 2 = 0,475
+    t = 1.96  # аргумент функции Лапласса, при котором ее значение равно 0,95 / 2 = 0,475
     e = t * sem(arr)
     mean_interval = (np.mean(arr) - e, np.mean(arr) + e)
 
     # для дисперсии
     # Случайная ошибка дисперсии нижней границы
-    X2 = 129.5612 # Для количества степеней свободы k = 99 и p = (1-0.95)/2 = 0.025 по таблице распределения χ2
+    X2 = 129.5612  # Для количества степеней свободы k = 99 и p = (1-0.95)/2 = 0.025 по таблице распределения χ2
     tH = (len(arr) - 1) * np.var(arr) / X2
     # Случайная ошибка дисперсии верхней границы
-    X2 = 74.22193 # Для количества степеней свободы k = 99 и p = (1-0.95)/2 = 0.975 по таблице распределения χ2
+    X2 = 74.22193  # Для количества степеней свободы k = 99 и p = (1-0.95)/2 = 0.975 по таблице распределения χ2
     tB = (len(arr) - 1) * np.var(arr) / X2
-    var_interval = (tH, tB)
+    var_interval = (np.sqrt(tH), np.sqrt(tB))
 
     return mean_interval, var_interval
+
 
 if __name__ == "__main__":
     stat = np.fromfile("sample.txt", dtype=int, sep=' ')
     print(stat)
 
-    # гистограмма и график функции распределения
-    # sns.distplot(stat, hist=True, kde=True, bins=10)
-    # plt.show()
+    # Гистограмма
+    sns.distplot(stat, hist=True, kde=True, bins=10)
+    plt.show()
+    # Эмперический график
+    plt.hist(stat, 100, normed=1, histtype='step', cumulative=True, label='Empirical')
+    plt.show()
+
+    print("Минимум: {}, максимум: {}".format(stat.min(), stat.max()))
 
     print("Среднее:", np.mean(stat))
 
-    print("Дисперсия:", np.var(stat))
+    print("Дисперсия:", np.var(stat, ddof=1))
 
     print("Стандртная ошибка:", sem(stat))
 
@@ -86,9 +130,9 @@ if __name__ == "__main__":
 
     print("Медиана:", np.median(stat))
 
-    # http://math.msu.su/~falin/files/Фалин_Г.И.,Фалин_А.И.(2011-Математика)Квартили_в_описательной_статистике.pdf
     print("Квартили:", quartil(stat), (np.percentile(stat, 25), np.percentile(stat, 50), np.percentile(stat, 75)))
 
+    # ящик с усами
     sns.boxplot(stat)
     plt.show()
 
@@ -96,10 +140,10 @@ if __name__ == "__main__":
 
     print("Эксцесс:", kurtosis(stat), st.kurtosis(stat))
 
-    # https: // ru.wikipedia.org / wiki / Коэффициент_асимметрии
     print("Асимметрия:", assimetria(stat), st.skew(stat))
 
-    print("Пирсон:", pirson(stat))
+    print("Пирсон:", pirson(stat, a=0.025, k=10, chi_crit=12.833))
 
-    print("Доверительные интервалы: для мат. ожидания:", interval(stat))
+    intervals = confidence_interval(stat)
+    print("Доверительные интервалы для мат. ожидания: {} и среднего квадратичного отклонения: {}:".format(intervals[0], intervals[1]))
     print(st.t.interval(0.95, len(stat)-1, loc=np.mean(stat), scale=st.sem(stat)))
