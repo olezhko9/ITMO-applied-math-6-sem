c10e36da9168c1f94280bbe3be1270a60c290b0e
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-04-07 23:29:48 +0300

    chi2 calculated by scipy.stats

--- lab 1/statistic.py
+++ lab 1/statistic.py
@@ -44,20 +44,19 @@ def laplass(x):
     return st.norm.cdf(x) - 0.5
 
 
-def pirson(arr, a, k, chi_crit):
+def pirson(arr, a, k):
     """
     Вычисляет хи-квадрат для заданной выборке
     :param arr: массив чисел - выборка
     :param a: уровень значимости
     :param k: количество интервалов разбиения
-    :param chi_crit: хи-квадрат критическое при аргументах (k-2-1, a)
     :return: True, если совокупность подчиняется нормальному закону, иначе False.
     Также возвращает вычисленное значение хи-квадрат.
     """
-    _k = k
+
     _min = np.min(arr)
     _max = np.max(arr)
-    h = (_max - _min) / _k
+    h = (_max - _min) / k
 
     x = _min
     k_intervals = {}
@@ -66,15 +65,17 @@ def pirson(arr, a, k, chi_crit):
         k_intervals[(x, xi)] = {'ni': 0}
         x = xi
 
-    for a in arr:
+    for x in arr:
         for interval in k_intervals:
-            if interval[0] <= a <= interval[1]:
+            if interval[0] <= x <= interval[1]:
                 k_intervals[interval]['ni'] += 1
                 break
 
     mean = np.mean(arr)
     std = stdev(arr)
     chisquare = 0
+    chi_crit = st.chi2.isf(q=a, df=k - 2 - 1)
+
     for interval in k_intervals:
         x1 = (interval[0] - mean) / std
         x2 = (interval[1] - mean) / std
@@ -83,28 +84,27 @@ def pirson(arr, a, k, chi_crit):
                      (len(arr) * k_intervals[interval]["pi"])
 
     if chisquare > chi_crit:
-        return False, chisquare
+        return False, chisquare, chi_crit
     else:
-        return True, chisquare
+        return True, chisquare, chi_crit
 
 
-def confidence_interval(arr):
+def confidence_interval(arr, y):
     """Доверительные интервалы"""
     # для математического ожидания
-    t = 1.96  # аргумент функции Лапласса, при котором ее значение равно 0,95 / 2 = 0,475
+    t = 1.96  # аргумент функции Лапласса, при котором ее значение равно y / 2 = 0,475
     e = t * sem(arr)
     mean_interval = (np.mean(arr) - e, np.mean(arr) + e)
 
-    # для дисперсии
-    # Случайная ошибка дисперсии нижней границы
-    X2 = 129.5612  # Для количества степеней свободы k = 99 и p = (1-0.95)/2 = 0.025 по таблице распределения χ2
-    tH = (len(arr) - 1) * np.var(arr) / X2
-    # Случайная ошибка дисперсии верхней границы
-    X2 = 74.22193  # Для количества степеней свободы k = 99 и p = (1-0.95)/2 = 0.975 по таблице распределения χ2
-    tB = (len(arr) - 1) * np.var(arr) / X2
-    var_interval = (np.sqrt(tH), np.sqrt(tB))
+    # для среднего квадратичного отклонения
+    X2 = st.chi2.isf(q=(1-y)/2, df=len(arr) - 1)  # Для количества степеней свободы k = 99 и p = (1-0.95)/2 = 0.025
+    tH = (len(arr) - 1) * np.var(arr, ddof=1) / X2
 
-    return mean_interval, var_interval
+    X2 = st.chi2.isf(q=(1+y)/2, df=len(arr) - 1)  # Для количества степеней свободы k = 99 и p = (1+0.95)/2 = 0.975
+    tB = (len(arr) - 1) * np.var(arr, ddof=1) / X2
+    std_interval = (np.sqrt(tH), np.sqrt(tB))
+
+    return mean_interval, std_interval
 
 
 if __name__ == "__main__":
@@ -112,7 +112,7 @@ if __name__ == "__main__":
     print(stat)
 
     # Гистограмма
-    sns.distplot(stat, hist=True, kde=True, bins=10)
+    sns.distplot(stat, hist=True, bins=10)
     plt.show()
     # Эмперический график
     plt.hist(stat, 100, normed=1, histtype='step', cumulative=True, label='Empirical')
@@ -142,8 +142,7 @@ if __name__ == "__main__":
 
     print("Асимметрия:", assimetria(stat), st.skew(stat))
 
-    print("Пирсон:", pirson(stat, a=0.025, k=10, chi_crit=12.833))
+    print("Пирсон:", pirson(stat, a=0.025, k=10))
 
-    intervals = confidence_interval(stat)
+    intervals = confidence_interval(stat, 0.95)
     print("Доверительные интервалы для мат. ожидания: {} и среднего квадратичного отклонения: {}:".format(intervals[0], intervals[1]))
-    print(st.t.interval(0.95, len(stat)-1, loc=np.mean(stat), scale=st.sem(stat)))
