49beae1cdd1b63d91a63a43bd128f733f1689619
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-04-25 15:45:16 +0300

    calculate non stationary coefficients step by step and plot them

--- lab_3/lab3.py
+++ lab_3/lab3.py
@@ -1,40 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from lab_2 import markov_chain
 
 # вариант 4
-# p = {
-#     'lambda': 7.92,     # Интенсивность входящего потока заявок
-#     'nu': 3.6,          # Интенсивность обслуживания заявки
-#     'r': 3,             # Количество каналов
-#     'm': 1              # Возможная длина очереди
-# }
-
-# пример
 p = {
-    'lambda': 6.07,     # Интенсивность входящего потока заявок
-    'nu': 3.1,          # Интенсивность обслуживания заявки
-    'r': 2,             # Количество каналов
-    'm': 2              # Возможная длина очереди
+    'lambda': 7.92,     # Интенсивность входящего потока заявок
+    'nu': 3.6,          # Интенсивность обслуживания заявки
+    'r': 3,             # Количество каналов
+    'm': 1              # Возможная длина очереди
 }
 
-print(p)
+# пример
+# p = {
+#     'lambda': 6.07,     # Интенсивность входящего потока заявок
+#     'nu': 3.1,          # Интенсивность обслуживания заявки
+#     'r': 2,             # Количество каналов
+#     'm': 2              # Возможная длина очереди
+# }
 
-
-# transition_matrix = np.array([
-#     np.array([-p['lambda'], p['nu'], 0, 0, 0]),
-#     np.array([p['lambda'], -(p['nu']+p['lambda']), 2*p['nu'], 0, 0]),
-#     np.array([0, p['lambda'], -(2*p['nu']+p['lambda']), 3*p['nu'], 0]),
-#     np.array([0, 0, p['lambda'], -(3*p['nu']+p['lambda']), 3*p['nu']]),
-#     np.array([0, 0, 0, p['lambda'], -3*p['nu']])
-# ])
 
 transition_matrix = np.array([
     np.array([-p['lambda'], p['nu'], 0, 0, 0]),
     np.array([p['lambda'], -(p['nu']+p['lambda']), 2*p['nu'], 0, 0]),
-    np.array([0, p['lambda'], -(2*p['nu']+p['lambda']), 2*p['nu'], 0]),
-    np.array([0, 0, p['lambda'], -(2*p['nu']+p['lambda']), 2*p['nu']]),
-    np.array([0, 0, 0, p['lambda'], -2*p['nu']])
+    np.array([0, p['lambda'], -(2*p['nu']+p['lambda']), 3*p['nu'], 0]),
+    np.array([0, 0, p['lambda'], -(3*p['nu']+p['lambda']), 3*p['nu']]),
+    np.array([0, 0, 0, p['lambda'], -3*p['nu']])
 ])
+
+# transition_matrix = np.array([
+#     np.array([-p['lambda'], p['nu'], 0, 0, 0]),
+#     np.array([p['lambda'], -(p['nu']+p['lambda']), 2*p['nu'], 0, 0]),
+#     np.array([0, p['lambda'], -(2*p['nu']+p['lambda']), 2*p['nu'], 0]),
+#     np.array([0, 0, p['lambda'], -(2*p['nu']+p['lambda']), 2*p['nu']]),
+#     np.array([0, 0, 0, p['lambda'], -2*p['nu']])
+# ])
 
 
 def solve_stationary_probability(transition_matrix):
@@ -53,7 +52,6 @@ def solve_stationary_probability(transition_matrix):
 
 stationary = solve_stationary_probability(transition_matrix)
 print("Стационарные вероятности: ", stationary)
-# print(list(map(lambda x: np.around(x, 6), stationary)))
 
 
 def average_machines(stationary_prob):
@@ -91,12 +89,12 @@ def average_timbers(stationary_prob):
 print("Среднее число бревен в очереди: %.2f\n\n" % average_timbers(stationary))
 
 
-def euler_method(transition_matrix, start_condition, t=2):
+def euler_method(transition_matrix, start_condition, stationary_prob):
     current_t = 0.0
-    h = 0.05
+    h = 0.02
     steps = [start_condition, ]
 
-    while current_t < t:
+    while markov_chain.vector_stdev(stationary_prob, steps[-1]) > 1e-3:
         line = []
         for i in range(len(start_condition)):
             line.append(steps[-1][i] + h * sum([x * steps[-1][j] for j, x in enumerate(transition_matrix[i])]))
@@ -107,21 +105,49 @@ def euler_method(transition_matrix, start_condition, t=2):
 
 
 start_cond = np.array([1, 0, 0, 0, 0])
-euler_steps = euler_method(transition_matrix, start_cond, t=2)
-print(euler_steps)
+euler_steps = euler_method(transition_matrix, start_cond, stationary)
+# print(euler_steps)
 
 
-def non_stationary_plot(steps):
-
+def non_stationary_plot(steps, labels):
     xmin = 0
-    xmax = 2
-    dx = 0.05
+    dx = 0.02
+    xmax = dx * len(steps)
 
-    xlist = np.arange(xmin, xmax + dx, dx)
-    for ylist in steps.T:
-        plt.plot(xlist, ylist)
+    xlist = np.arange(xmin, xmax, dx)
+    for i, ylist in enumerate(steps.T):
+        plt.plot(xlist, ylist, label=labels[i])
 
+    plt.gca().yaxis.grid(True)
+    plt.yticks([x for x in np.arange(0, 1, 0.1)])
+    plt.legend()
     plt.show()
 
 
-non_stationary_plot(euler_steps)
+# график вероятности состояний системы
+non_stationary_plot(euler_steps, labels=['p0(t)', 'p1(t)', 'p2(t)', 'p3(t)', 'p4(t)'])
+
+
+def non_stationary_coefs(steps):
+    coef_steps = []
+    i = 0
+
+    while i < len(steps):
+        coef_work = sum(k * steps[i][k] for k in range(0, p['r']))
+        coef_work += p['r'] * sum(steps[i][k] for k in range(p['r'], p['r'] + p['m'] + 1))
+        coef_work /= p['r']
+
+        coef_idle = sum(k * steps[i][p['r'] - k] for k in range(1, p['r'] + 1)) / p['r']
+
+        coef_steps.append([coef_work, coef_idle])
+        i += 1
+
+    return np.array(coef_steps)
+
+
+coefficients = non_stationary_coefs(euler_steps)
+# print(coefficients)
+
+
+# график коэффициентов загрузки и простоя машин
+non_stationary_plot(coefficients, labels=['Загрузка', 'Простой'])
